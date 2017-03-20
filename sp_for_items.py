5ad84529d190767625977da7e57fd7505c5d2334
[NHibiki1] <[wl1508@nyu.edu]>
2017-03-20 14:05:11 +0800

    数据录入,时间，菜是否在套餐中（没有爬菜的数据库）

--- sp_for_items.py
+++ sp_for_items.py
@@ -1,10 +1,13 @@
-import xlrd
+import datetime
+import time
 
 
-data = xlrd.open_workbook('excelFile.xls')
-table = data.sheets()[0]
+def Caltime(date1,date2):
+    date1=time.strptime(date1,"%Y-%m-%d %H:%M:%S")
+    date2=time.strptime(date2,"%Y-%m-%d %H:%M:%S")
+    date1=datetime.datetime(date1[0],date1[1],date1[2],date1[3],date1[4],date1[5])
+    date2=datetime.datetime(date2[0],date2[1],date2[2],date2[3],date2[4],date2[5])
+    return date2-date1
 
-for i in range(10):
-    for y in range(10):
-        table.put_cell(i, y, 1, i, 1)
-
+print(Caltime( '2017-03-20 10:36:37', '2017-03-27 10:38:52').total_seconds())
+print(Caltime( '2017-03-20 10:36:37', '2017-03-27 10:38:52') > 7)
--- spyder.py
+++ spyder.py
@@ -1,7 +1,8 @@
 import requests as R
 import json
+import time
 import datetime
-
+import re
 
 
 
@@ -21,30 +22,42 @@ bombreq = R.get("http://www.weimiaoqu.com/index.php?g=User&m=Store&a=orders&toke
 
 bill_list = []
 next_item = 0
+dt = []
+next_time = 0
 
-while (next_item != -1):
+while next_item != -1:
     next_item = bombreq.find('"showIntroDetail',next_item+1)
-    if (next_item != -1):
+    if next_item != -1:
         bill_list.append(bombreq[next_item+17:bombreq.find(')',next_item+1)])
-
+while next_time != -1:
+    next_time = bombreq.find("<td>", next_time+1)
+    if next_time != -1 and len(bombreq[next_time+4:bombreq.find('</td>', next_time+1)]) == 19:
+        dt.append(bombreq[next_time+4:bombreq.find('</td>', next_time+1)])
 items = []
+count = 0
 for i in bill_list:
     item = R.get("http://www.weimiaoqu.com/index.php?g=User&m=Store&a=orderInfo&token=axdaze1485238944&dining=0&id=" + i,cookies={"PHPSESSID":session}).text
     it = {}
+    ip = []
     for j in item.split("\r\n"):
+
         if j[-5:] == '</td>' and '<br/>' not in j:
             name = j.find('<br>')
-            it['物品'] = j[name+4:-5]
+            ip.append(j[name+4:-5])
         if j[-4:] == "<br>" and "<span" not in j:
             it[j.split("：")[0].strip()] = j.split("：")[1].strip()[0:-4]
     if it != {}:
+        it['valid'] = 1
+        it['时间'] = dt[count]
+        it['物品'] = ip
+        count += 1
         items.append(it)
 
-# print(items)
-# for i in items:
-#     for k in i:
-#         print(k, ':', i[k])
-#     print('------------------------------')
+print(items)
+for i in items:
+    for k in i:
+        print(k, ':', i[k])
+    print('------------------------------')
 
 A = '奢华'
 B = '亲民'
@@ -54,20 +67,54 @@ list_plan = [[], [], []]
 list_dishes = [[], [], []]
 
 for item in items:
-    for name in item:
-        if A in item[name] and '-' not in item[name]:
+        if A in item['物品'] and '-' not in item['物品']:
             list_plan[0].append(item)
-        elif B in item[name] and '-' not in item[name]:
+        elif B in item['物品'] and '-' not in item['物品']:
             list_plan[1].append(item)
-        elif C in item[name] and '-' not in item[name]:
+        elif C in item['物品'] and '-' not in item['物品']:
             list_plan[2].append(item)
 
-        elif A in item[name]:
+        elif A in item['物品']:
             list_dishes[0].append(item)
-        elif B in item[name]:
+        elif B in item['物品']:
             list_dishes[1].append(item)
-        elif C in item[name]:
+        elif C in item['物品']:
             list_dishes[2].append(item)
 
+DURATION = 7
+
+def caltime(date1):
+    date1 = time.strptime(date1,"%Y-%m-%d %H:%M:%S")
+    date2 = datetime.datetime.now()
+    date1 = datetime.datetime(date1[0],date1[1],date1[2],date1[3],date1[4],date1[5])
+    return date2-date1
+
+
+for plan in list_plan:
+    for one in plan:
+         if caltime(one['时间']).total_seconds() / 86400 > DURATION:
+             one['valid'] = 0
+
+
+invalid_request = []
+valid_request = []
+
+dish_base = [[], [], []]
+
+
+for rank in range(len(list_plan)):
+    for mem in list_plan[rank]:
+        if len(mem['物品']) != int(mem['总数']):
+            for dish in list_dishes:
+                if dish['电话'] == mem['电话']:
+                    invalid_request.append(dish)
+                    break
+        else:
+            for dish in list_dishes:
+                if dish['电话'] == mem['电话']:
+                    if dish['物品'] in dish_base[rank]:
+                        valid_request.append(dish)
+                    else:
+                        invalid_request.append(dish)
 
 
